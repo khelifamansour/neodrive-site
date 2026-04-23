6eb55586e0f0cd84d4fe0fd31ab8036e9957f4a0
khelifamansour <khelifamansour@hotmail.fr>
2026-04-23 19:02:01 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,8 @@ export default function RootLayout({
             <a href="/livraison">Livraison</a>
             <a href="/carte-grise">Carte grise</a>
             <a href="/faq">FAQ</a>
+            <a href="/QuiSommesNous">QuiSommesNous</a>
+            
             <a href="/contact">Contact</a>
           </div>
         </nav>
