1958f19146ec41aa821d8c10b9d7cfba4559cdc1
khelifamansour <khelifamansour@hotmail.fr>
2026-05-17 19:07:25 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,14 +1,6 @@
-import { createClient }
-from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl =
-"https://tzlsdjzcxdjaatcpwqwn.supabase.co";
-
-const supabaseKey =
-"sb_publishable_FxvXFqvTpjdu3vYbCQo9qQ_lTlNrAMd";
-
-export const supabase =
-createClient(
-  supabaseUrl,
-  supabaseKey
+export const supabase = createClient(
+  "https://tzlsdjzcxdjaatcpwqwn.supabase.co",
+  "sb_publishable_FxvXFqvTpjdu3vYbCQo9qQ_lTlNrAMd"
 );
