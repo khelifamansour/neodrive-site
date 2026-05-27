81cd5bf15fb63ed5fff6b991c801bdeb26f11ceb
khelifamansour <khelifamansour@hotmail.fr>
2026-05-27 18:17:01 +0100

    Update page.tsx

--- app/cmr/page.tsx
+++ app/cmr/page.tsx
@@ -207,7 +207,7 @@ export default function CRMPage() {
           (row: string) => {
 
           /* =========================
-             SIMPLE CSV PARSER
+             CSV PARSER
           ========================= */
 
           const cols =
