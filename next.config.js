8e307e0e7783bbab4d2e920a157eb70019d84ff8
khelifamansour <khelifamansour@hotmail.fr>
2026-05-28 15:43:35 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 
 const nextConfig = {
 
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
 };
 
-export default nextConfig;
+module.exports = nextConfig;
