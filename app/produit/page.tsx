e53e4e1628a52020f7aeb4948cc9ebe27c68f5b0
khelifamansour <khelifamansour@hotmail.fr>
2026-06-01 23:49:26 +0100

    Update page.tsx

--- app/produit/page.tsx
+++ app/produit/page.tsx
@@ -32,7 +32,7 @@ export default function Produit() {
         </p>
 
         <h2 style={price}>
-          À partir de 5490 €
+          À partir de 5 990 €
         </h2>
 
         <div style={center}>
