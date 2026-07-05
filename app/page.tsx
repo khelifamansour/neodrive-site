ecb693bf5c4fd19adf5b0897c64b54ed35427bc2
khelifamansour <khelifamansour@hotmail.fr>
2026-07-05 19:44:12 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -237,6 +237,16 @@ export default function Home() {
   <span>SIREN 908 645 393</span>
 </footer>
 
+      .footer a {
+  color: #111;
+  font-weight: 800;
+  text-decoration: none;
+}
+
+.footer a:hover {
+  text-decoration: underline;
+}
+
       <style jsx global>{`
         html, body {
           margin: 0;
