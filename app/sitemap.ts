9ef2116643a5c0c3a6e4212d0a9a1e9ad1df5e5a
khelifamansour <khelifamansour@hotmail.fr>
2026-07-05 19:30:08 +0100

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.95,
     },
     {
+  url: `${baseUrl}/guide-voiture-sans-permis`,
+  lastModified: new Date(),
+  changeFrequency: "weekly",
+  priority: 0.9,
+},
+    {
       url: `${baseUrl}/QuiSommesNous`,
       lastModified: new Date(),
       changeFrequency: "monthly",
